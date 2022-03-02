d33febc523105398c4a3599c81d3e94dcc5e5f02
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-03-02 07:28:32 +0100

    added classes

---
+++ models/movable-object.class.js
@@ -0,0 +1,13 @@
+class movableObject {
+    x;
+    y;
+    img;
+
+    moveRight() {
+        console.log('moving right');
+    }
+
+    moveRight() {
+        console.log('moving right');
+    }
+}
---
+++ models/world.class.js
@@ -0,0 +1,13 @@
+class World {
+    character = new Character();
+    enemies = [
+        new chicken(),
+        new chicken(),
+        new chicken(),
+    ];
+
+
+    draw() {
+        
+    }
+}
