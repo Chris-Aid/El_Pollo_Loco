c2be2edbb22a04dc13a8a66cb7285d2f1ddf22af
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-03-26 13:16:00 +0100

    implemented images of character

--- models/charakter.class.js
+++ models/charakter.class.js
@@ -4,9 +4,29 @@ class Character extends movableObject {
     width = 120;
     y = 155;
     x = 10;
+    imagesWalking = [
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-23.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-24.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor() {
-        super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/1.IDLE/IDLE/I-1.png')
+        super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/1.IDLE/IDLE/I-1.png');
+        this.loadImages(this.imagesWalking)
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.imagesWalking.length;
+            this.loadImage(this.imagesWalking[i])
+            this.currentImage++;
+        }, 100);
     }
 
     jump() {
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,19 @@ class movableObject {
     // img;
     // height = 150;
     // width = 100;
+    imageCache = {}
 
     loadImage(path) {
-        this.img = new Image();
+        this.img = new Image(); // this.img = document.getElementbyId('image') <img id="image">
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
